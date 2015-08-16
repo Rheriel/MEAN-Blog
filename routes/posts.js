bc97f4100b024205fb7b7bcebbe373a0c6ebb225
Rheriel <rheriel@hotmail.co.jp>
2015-08-16 00:39:42 -0500

    Layout overrides
    Posts.jade overrides title from Layout

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET posts listing. */
 router.get('/', function(req, res, next) {
-  res.render('posts');
+  res.render('posts', { pageTitle: 'HalfMind - Dev Blog' });
 });
 
 module.exports = router;
